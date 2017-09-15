cda171ce7cc59f47706c91c2e2cdb705a6a3963b
brianwang9100@gmail.com <brianwang9100@gmail.com>
2017-09-14 20:23:58 -0400

    Added additional comments to tableview

--- HackGTeenyChatApp/ViewController.swift
+++ HackGTeenyChatApp/ViewController.swift
@@ -12,13 +12,13 @@ import FirebaseDatabase
 
 class ViewController: UIViewController {
     
-    //nil reference to remote firebase database
+    // reference to the firebase database
     var root: DatabaseReference!
     
-    //backing array for tableView
+    // backing messages array for tableView
     var messages:[String] = []
 
-    //connections to UI
+    // connections to UI
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextField: UITextField!
     
@@ -43,15 +43,26 @@ class ViewController: UIViewController {
     }
 }
 
+// this is an extension, it allows you to write additional methods and variables to a preexisting class.
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
+    // this delegate method tells the tableview how many rows it has.
+    // in this case, we want the tableview to have the same number of rows as there are messages.
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return messages.count
     }
 
+    // this delegate method tells the tableview what content goes into each row.
+    // this is called messages.count number of times, one for each message
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // dequeue the protoype cell from main.storyboard and assign it to 'cell'
         let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell", for: indexPath)
+        
+        // set the label to the message indexed from messages
         cell.textLabel?.text = messages[indexPath.row]
+        
+        // give the cell to tableview to display
         return cell
     }
 }
